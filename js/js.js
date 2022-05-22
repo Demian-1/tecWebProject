12a36d7555601b11d9b375a25694175168ff4e7d
Luis <luise.ascencio13@gmail.com>
2022-05-22 18:38:59 -0500

    Formulario de varios pasos

--- js/js.js
+++ js/js.js
@@ -1,10 +1,52 @@
-function calcularEdad() 
-{
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-nxt");
+const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step");
+
+let formStepsNum = 0;
+
+nextBtns.forEach(btn =>{
+	btn.addEventListener("click", () => {
+		formStepsNum++;
+		updateFormsSteps();
+		updateProgressBar();
+	});
+});
+
+prevBtns.forEach(btn =>{
+	btn.addEventListener("click", () => {
+		formStepsNum--;
+		updateFormsSteps();
+		updateProgressBar();
+	});
+});
+
+function updateFormsSteps(){
+	formSteps.forEach(formStep =>{
+		formStep.classList.contains("form-step-active") &&
+		formStep.classList.remove("form-step-active");
+	});
+	formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+function updateProgressBar(){
+	progressSteps.forEach((progressSteps,idx)=>{
+		if(idx <= formStepsNum){
+			progressSteps.classList.add("progress-step-active");
+		}
+		else{
+			progressSteps.classList.remove("progress-step-active");
+		}
+	});
+	
+	const progressActive = document.querySelectorAll(".progress-step-active");
+	progress.style.width = ((progressActive.length - 1)/(progressSteps.length - 1)*100 + "%");
+}
+
+function calcularEdad() {
   	const date = new Date(document.getElementById('kid_birthday').value).getTime();
-  	alert(date);
   	const now = new Date().getTime();
-  	alert(now);
   	var age = Math.floor((now - date ) / (1000 * 60 * 60 * 24 * 365));
-  	alert(age);
     (document.getElementById('kid_age')).value = age;  
 }
