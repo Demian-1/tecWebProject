7afef5016399f97cc50c1746767560951b67b870
Luis <luise.ascencio13@gmail.com>
2022-06-05 02:40:36 -0500

    Validaciones al 90 por ciento

--- js/js.js
+++ js/js.js
@@ -5,8 +5,6 @@ const formSteps = document.querySelectorAll(".form-step");
 const progressSteps = document.querySelectorAll(".progress-step");
 const hasPartner = document.getElementById("der_cony_no");
 
-const folio = document.getElementById("folio");
-
 let formStepsNum = 0;
 
 nextBtns.forEach(btn =>{
@@ -20,6 +18,7 @@ nextBtns.forEach(btn =>{
 			}else{
 				formStepsNum++;
 			}
+			console.log("estamos en: " + formStepsNum);
 			updateFormsSteps();
 			updateProgressBar();
 		}
@@ -41,24 +40,93 @@ prevBtns.forEach(btn =>{
 function checkVal(){
 	let errors = 0;
 	let campos = document.querySelectorAll(".form-step-active .needs-validation");
+	const regExTxt = /^[a-zA-Z\s]*$/;
+	const regExFol = /(?:P[PE]|\d\d)\d\d\d\d\d\d\d\d/i;
+	const regExCURP = /[A-Z][A-Z][A-Z][A-Z]\d\d\d\d\d\d[A-Z][A-Z][A-Z][zA-Z][A-Z][A-Z][A-Z0-9][A-Z0-9]/gi;
 	for( i = 0; i < campos.length ; i++){
+		campos[i].classList.contains("is-invalid") && campos[i].classList.remove("is-invalid");
+		if(campos[i].id.endsWith("pat") || campos[i].id.endsWith("mat") || campos[i].id.endsWith("nombres") || campos[i].id.endsWith("calle") || campos[i].id.endsWith("plaza")){
+			if(!regExTxt.test(campos[i].value)){
+				campos[i].classList.add("is-invalid");
+				errors++;
+			}
+		}
+		else if(campos[i].id == "folio"){
+			if(campos[i].value.length < 10 || !regExFol.test(campos[i].value)){
+				campos[i].classList.add("is-invalid");
+				errors++;
+			}
+		}
+		else if(campos[i].id.endsWith("CURP")){
+			if(campos[i].value.length < 18 || !regExCURP.test(campos[i].value)){
+				campos[i].classList.add("is-invalid");
+				errors++;
+			}
+		}
+		else if(campos[i].id == "kid_age"){
+			if(campos[i].value > 6 || campos[i].value < 0){
+				campos[i].classList.add("is-invalid");
+				document.getElementById("invalid-age").style.display = "block";
+				errors++;
+			}
+			else{
+				document.getElementById("invalid-age").style.display = "none";
+			}
+		}
 		if(!campos[i].checkValidity()){
 			campos[i].classList.add("is-invalid");
 			errors++;
-		}
-		else{
-			campos[i].classList.contains("is-invalid") && campos[i].classList.remove("is-invalid");
 		}
 	}
 	return errors;
 }
 
 function updateFormsSteps(){
+	const revision = document.getElementById("revision");
+	const btnsgroup = document.querySelectorAll(".btns-group.remove-on-check");
+	const inputs = document.querySelectorAll(".form-control:not(#kid_age,#kid_age_months,[id$='entidad'],[id$='alcaldia'],[id$='colonia']),.form-select");
+	const btns_revision = document.getElementById("btns-revision");
+	const hide_on_revision = document.querySelectorAll(".hide-on-revision");
+
 	formSteps.forEach(formStep =>{
 		formStep.classList.contains("form-step-active") &&
 		formStep.classList.remove("form-step-active");
 	});
-	formSteps[formStepsNum].classList.add("form-step-active");
+	// Parte de la revision de información
+	if(formStepsNum == 4){
+		revision.classList.remove("d-none");
+		btns_revision.classList.remove("d-none");
+		formSteps.forEach(formStep =>{
+			formStep.classList.add("form-step-active");
+		});
+		if(hasPartner.checked == true){
+			document.getElementById("conyuge").classList.remove("form-step-active");
+		}
+		hide_on_revision.forEach(hide_on_revision =>{
+			hide_on_revision.classList.add("d-none");
+		});
+		btnsgroup.forEach(btnsgroup =>{
+			btnsgroup.classList.add("d-none");
+		});
+		inputs.forEach(inputs =>{
+			inputs.setAttribute('disabled','');
+		});
+	}
+	// Avance del formulario normalmente
+	else{
+		revision.classList.add("d-none");
+		btns_revision.classList.add("d-none");
+		hide_on_revision.forEach(hide_on_revision =>{
+			hide_on_revision.classList.remove("d-none");
+		});
+		btnsgroup.forEach(btnsgroup =>{
+			btnsgroup.classList.remove("d-none");
+		});
+		inputs.forEach(inputs =>{
+			inputs.removeAttribute('disabled');
+		});
+		formSteps[formStepsNum].classList.add("form-step-active");
+	}
 }
 
 function updateProgressBar(){
@@ -74,6 +142,13 @@ function updateProgressBar(){
 	const progressActive = document.querySelectorAll(".progress-step-active");
 	progress.style.width = ((progressActive.length - 1)/(progressSteps.length - 1)*100 + "%");
 }
+
+document.getElementsByTagName("form")[0].onkeypress = function(e) {
+    var key = e.charCode || e.keyCode || 0;     
+    if (key == 13) {
+      e.preventDefault();
+    }
+  }
 
 function calcularEdad() {
   	const date = new Date(document.getElementById('kid_birthday').value).getTime();
