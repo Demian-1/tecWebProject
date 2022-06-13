d32293b5042e9f7acc4a2df85e75dcc6a7d6650b
35DR45 <esdras.josue@hotmail.com>
2022-06-13 11:48:40 -0500

    Vaina CRUD

--- js/jsCRUDO.js
+++ js/jsCRUDO.js
@@ -1,12 +1,54 @@
 $(document).ready(
     function(){
-    
-    $.ajax ({method:"POST", url:"CRUDR.php", cache:false, success:function(res){ $("#container").html(res)}});
-    $("#recargar").submit(function(e){
+
+    $("#fol").hide();
+    $("#table").hide();
+    $("#sub").hide();
+
+    $( "#Tipo" ).change(function() {
+        var acc= $("#Tipo").val();
+        switch (acc){ 
+            case 'D':
+                $("#fol").show();
+                $("#table").hide();
+                $("#sub").hide();
+            break;
+            case 'U':
+                $("#fol").show();
+                $("#table").hide();
+                $("#sub").hide();
+            break;
+            case 'R':
+                $("#table").show();
+                $("#fol").hide();
+                $("#sub").hide();
+            break;
+            case 'C':
+                $("#table").hide();
+                $("#fol").hide();
+                $("#sub").show();
+            break;
+        }
+        
+        alert( acc );
+    });
+
+    $("#table").change(function(){
+        $("#sub").show();
+    });
+
+    $("#fol").change(function(){
+        $("#sub").show();
+    });
+
+    /*$.ajax ({method:"POST", url:"CRUDR.php", cache:false, success:function(res){ $("#result").html(res)}});*/
+
+    $("#accion").submit(function(e){
         e.preventDefault();
         $.ajax ({method:"POST", url:"CRUDR.php", cache:false, success:function(res){ $("#container").html(res)}});
         
     });
+
     $("#eliminar").submit(function(e){
         e.preventDefault();
         var fol = $("#fol").val();
@@ -14,4 +56,5 @@ $(document).ready(
         //$.ajax ({method:"POST", url:"CRUDD.php", data:{fol:fol}, cache:false, success:function(res){ $("#container").html(res)}});
         
     });
+
 });
