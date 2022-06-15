59b35caac5fac1d275c50c12939e1782d6ade968
Luis <luise.ascencio13@gmail.com>
2022-06-15 00:24:48 -0500

    finalmente se envian a la bd

--- form-base/form-base.php
+++ form-base/form-base.php
@@ -6,6 +6,7 @@
 
     $folio=$_POST['folio'];
     $cendi=$_POST['cendi'];
+    $kid_img=$_POST['kid_img'];
     $kid_apellido_pat=$_POST['kid_apellido_pat'];
     $kid_apellido_mat=$_POST['kid_apellido_mat'];
     $kid_nombres=$_POST['kid_nombres'];
@@ -17,9 +18,13 @@
     $kid_cita=$_POST['kid_cita'];
     $kid_entrevista=$_POST['kid_entrevista'];
     $kid_grupo=$_POST['kid_grupo'];
+    $kid_dia_cita=NULL; //cambiar
+    $kid_dia_entrevista=NULL; //cambiar
+    $resp_img=$_POST['resp_img'];
 
     // Derechohabiente datos:
 
+    $der_img=$_POST['der_img'];
     $der_apellido_pat=$_POST['der_apellido_pat'];
     $der_apellido_mat=$_POST['der_apellido_mat'];
     $der_nombres=$_POST['der_nombres'];
@@ -46,6 +51,7 @@
 
     // Conyuge Datos
 
+    $cony_img=$_POST['cony_img'];
     $cony_apellido_pat=$_POST['cony_apellido_pat'];
     $cony_apellido_mat=$_POST['cony_apellido_mat'];
     $cony_nombres=$_POST['cony_nombres'];
@@ -63,18 +69,17 @@
     $cony_tel_trabajo=$_POST['cony_tel_trabajo'];
     $cony_extension=$_POST['cony_extension'];
     $cony_si=$_POST['cony_si'];
-    $cony_curp=$_POST['cony_curp'];
     $cony_religion=$_POST['cony_religion'];
 
-    $sql_n="INSERT INTO `DBTECWEB`.`MORRO` (`FOLIO`, `CENDI_idCENDI`, `APE_PAT`, `APE_MAT`, `NOM_M`, `FECHAN`, `EDAD`, `CURP`, `FOTO_M`, `2PADRES`, `CITAS_idCITAS`, `ENTREVISTA_idENTREVISTA`, `GRUPO_idGRUPO`) VALUES ('$folio', '$cendi', '$kid_apellido_pat', '$kid_apellido_mat', '$kid_nombres', '$kid_birthday', '$kid_age', '$kid_CURP', '$kid_foto', '$kid_dos_padres', '$kid_cita', '$kid_entrevista', '$kid_grupo')";
-    $sql_d="INSERT INTO `DBTECWEB`.`DERECHOHABIENTE` (`MORRO_Boleta_D`, `APE_PAT_D`, `APE_MAT_D`,`FOTO_D`, `NOM_D`,`AUT_D`,`FOTOA_D`, `NUM_EXT_D`, `NUM_INT_D`, `CALLE_D`, `TELF_D`, `TELC_D`, `MAIL_D`, `OCUPACION_ID`, `CURP_D`, `PLAZA_D`, `SUELDO_D`, `NUMEROE_D`, `ADSCRIPCION_idADS`, `NOMJEFE_D`, `CARGOJEFE_D`, `HORARIO_idHORARIO`, `EXT_D`, `COLONIA_D`, `MUNICIPIO_D`, `ESTADO_D`, `CP_D`) VALUES ('$folio', '$der_apellido_pat', '$der_apellido_mat','default.png', '$der_nombres',1,'default.png','$der_numExt','$der_numInt', '$der_calle', '$der_tel_fijo', '$der_tel_celular', '$der_email', '$der_ocupacion', '$der_CURP', '$der_plaza', '$der_sueldo', '$der_numEmpleado', '$der_adscripcion', '$der_jefe', '$der_jefe_puesto', '$der_horario', '$der_extension', '$der_colonia', '$der_alcaldia', '$der_entidad', '$der_cp')";
-    $sql_c="INSERT INTO `DBTECWEB`.`CONYUGE` (`MORRO_Boleta_C`, `APE_PAT_C`, `APE_MAT_C`,`NOM_C`,`FOTO_C`, `CALLE_C`, `NUM_INT_C`, `NUM_EXT_C`, `LUGART_C`, `DOMT_C`, `TELT_C`, `TELC_C`, `RELIGION_C`, `EXT_C`, `CURP_C`, `COLONIA_C`, `MUNICIPIO_C`, `ESTADO_C`, `CP_C`) VALUES ('$folio', '$cony_apellido_pat', '$cony_apellido_mat', '$cony_nombres','default.png', '$cony_calle', '$cony_numInt', '$cony_numExt', '$cony_lugar_trabajo', '$cony_domicilio_trabajo', '$cony_tel_trabajo', '$cony_tel_celular', '$cony_religion', '$cony_extension', '$cony_curp', '$cony_colonia', '$cony_alcaldia', '$cony_entidad', '$cony_cp')";
+    $sql_n="INSERT INTO `DBTECWEB`.`MORRO` (`FOLIO`, `CENDI_idCENDI`, `APE_PAT`, `APE_MAT`, `NOM_M`, `FECHAN`, `EDAD`, `CURP`, `FOTO_M`, `2PADRES`, `CITAS_idCITAS`, `ENTREVISTA_idENTREVISTA`, `GRUPO_idGRUPO`, `DIA_CITA`, `DIA_ENTREVISTA`,`FOTO_AUT`) VALUES ('$folio', '$cendi', '$kid_apellido_pat', '$kid_apellido_mat', '$kid_nombres', '$kid_birthday', '$kid_age', '$kid_CURP', '$kid_img', '$kid_dos_padres', '$kid_cita', '$kid_entrevista', '$kid_grupo','$kid_dia_cita','$kid_dia_entrevista','$resp_img')";
+    $sql_d="INSERT INTO `DBTECWEB`.`DERECHOHABIENTE` (`MORRO_Boleta_D`, `APE_PAT_D`, `APE_MAT_D`,`FOTO_D`, `NOM_D`,`AUT_D`,`FOTOA_D`, `NUM_EXT_D`, `NUM_INT_D`, `CALLE_D`, `TELF_D`, `TELC_D`, `MAIL_D`, `OCUPACION_ID`, `CURP_D`, `PLAZA_D`, `SUELDO_D`, `NUMEROE_D`, `ADSCRIPCION_idADS`, `NOMJEFE_D`, `CARGOJEFE_D`, `HORARIO_idHORARIO`, `EXT_D`, `COLONIA_D`, `MUNICIPIO_D`, `ESTADO_D`, `CP_D`) VALUES ('$folio', '$der_apellido_pat', '$der_apellido_mat','$der_img', '$der_nombres',1,'default.png','$der_numExt','$der_numInt', '$der_calle', '$der_tel_fijo', '$der_tel_celular', '$der_email', '$der_ocupacion', '$der_CURP', '$der_plaza', '$der_sueldo', '$der_numEmpleado', '$der_adscripcion', '$der_jefe', '$der_jefe_puesto', '$der_horario', '$der_extension', '$der_colonia', '$der_alcaldia', '$der_entidad', '$der_cp')";
+    $sql_c="INSERT INTO `DBTECWEB`.`CONYUGE` (`MORRO_Boleta_C`, `APE_PAT_C`, `APE_MAT_C`,`NOM_C`,`FOTO_C`, `CALLE_C`, `NUM_INT_C`, `NUM_EXT_C`, `LUGART_C`, `DOMT_C`, `TELT_C`, `TELC_C`, `RELIGION_C`, `EXT_C`, `COLONIA_C`, `MUNICIPIO_C`, `ESTADO_C`, `CP_C`) VALUES ('$folio', '$cony_apellido_pat', '$cony_apellido_mat', '$cony_nombres','$cony_img', '$cony_calle', '$cony_numInt', '$cony_numExt', '$cony_lugar_trabajo', '$cony_domicilio_trabajo', '$cony_tel_trabajo', '$cony_tel_celular', '$cony_religion', '$cony_extension', '$cony_colonia', '$cony_alcaldia', '$cony_entidad', '$cony_cp')";
 
     $res_n = mysqli_query($conexion,$sql_n);
     $res_d = mysqli_query($conexion,$sql_d);
 
     // echo $res_d;
-    if($cony_curp==''){
+    if($cony_nombres==''){
         echo $res_d and $res_n;
     }else{
         echo mysqli_query($conexion,$sql_c);
