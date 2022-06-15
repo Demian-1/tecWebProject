5e968943f4c0c830d12744fd201fcbecd87e11c9
35DR45 <104373156+35DR45@users.noreply.github.com>
2022-06-15 03:45:42 -0500

    CRUD "TERMINADO"

--- CRUDR.php
+++ CRUDR.php
@@ -44,7 +44,7 @@
 	    	$res=$res."<tr>
 	    		<th scope=row><center> ". $row["FOLIO"]." </center></td>
 	    		<td><center> " . $row["NOM_M"]." ".$row["APE_PAT"]." ".$row["APE_MAT"]. " </center></td>
-	    		<td><center><img src='img/" . $row["FOTO_M"]. "' width=75%/></center></td>
+	    		<td><center><img src='img/customerImages/" . $row["FOTO_M"]. ".png' width=75%/></center></td>
 	    		<td><center>" .$row["EDAD"]. "</center></td> 
 	    		<td><center>" .$bipater. "</center></td> 
 	    		<td><center>" .$row["NOMBRE_G"]. "</center></td>
@@ -54,12 +54,12 @@
 	    	  </tr>";
 	  		}
 	    }elseif ($table=="DERECHOHABIENTE") {
-			$res="<center><table class='table' style='background-color: white; border-radius: 1%; width: 88%'> <thead> <th> FOLIO </th> <th scope='col'> NOMBRE </th><th scope='col'><center> FOTO </center></th><th scope='col'> DIRECCION </th><th scope='col'> OCUPACION </th><th scope='col'> PLAZA </th>  <th scope='col'> SUELDO </th> <th scope='col'> NUMERO DE EMPLEADO </th> <th scope='col'> ADSCRIPCION </th><th scope='col'> JEFE </th><th scope='col'> HORARIO </th><th scope='col'> TELEFONO CELULAR</th><th scope='col'> TELEFONO FIJO </th><th scope='col'> CURP </th></thead>";
+			$res="<center><table class='table' style='background-color: white; border-radius: 1%; width: 88%'> <thead> <th> FOLIO </th> <th scope='col'> NOMBRE </th><th scope='col'><center> FOTO </center></th><th scope='col'> DIRECCION </th><th scope='col'> OCUPACION </th><th scope='col'> PLAZA </th>  <th scope='col'> SUELDO </th> <th scope='col'> NUMERO DE EMPLEADO </th> <th scope='col'> ADSCRIPCION </th><th scope='col'> JEFE </th><th scope='col'> HORARIO </th><th scope='col'> TELEFONO CELULAR</th><th scope='col'> TELEFONO FIJO </th><th scope='col'> CURP </th><th scope='col'> MAIL </th></thead>";
 			while($row = $result->fetch_assoc()) {
 			$res=$res."<tr>
 	    		<th scope=row><center> ". $row["MORRO_Boleta_D"]." </center></td>
 	    		<td><center> " . $row["NOM_D"]." ".$row["APE_PAT_D"]." ".$row["APE_MAT_D"]. " </center></td>
-	    		<td><center><img src='img/" . $row["FOTO_D"]. "' width=75%/></center></td>
+	    		<td><center><img src='img/customerImages/" . $row["FOTO_D"]. "' width=75%/></center></td>
 	    		<td><center>" .$row["CP_D"]." ".$row["ESTADO_D"]." ".$row["MUNICIPIO_D"]." ".$row["COLONIA_D"]." ".$row["NUM_EXT_D"]." ".$row["NUM_INT_D"]. "</center></td> 
 	    		<td><center>" .$row["NOM_O"]. "</center></td> 
 	    		<td><center>" .$row["PLAZA_D"]. "</center></td>
@@ -71,28 +71,27 @@
 	    		<td><center>" .$row["TELC_D"]." ". "</center></td>
 	    		<td><center>" .$row["TELF_D"]." ".$row["EXT_D"]. "</center></td>
 	    		<td><center>" .$row["CURP_D"]. "</center></td>
+	    		<td><center>" .$row["MAIL_D"]. "</center></td>
 	    	  </tr>";
 	  		} 
 			/**/
 	    }elseif ($table=="CONYUGE"){
-	    	$res= "<center><table class='table' style='background-color: white; border-radius: 1%; width: 88%'> <thead> <th> FOLIO </th> <th scope='col'> NOMBRE </th><th scope='col'><center> FOTO </center></th><th scope='col'> DIRECCION </th><th scope='col'> LUGAR DE TRABAJO </th>  <th scope='col'> TELEFONO TRABAJO </th> <th scope='col'> TELEFONO CASA </th> <th scope='col'> RELIGION </th><th scope='col'> CURP </th></thead>";
+	    	$res= "<center><table class='table' style='background-color: white; border-radius: 1%; width: 88%'> <thead> <th> FOLIO </th> <th scope='col'> NOMBRE </th><th scope='col'><center> FOTO </center></th><th scope='col'> DIRECCION </th><th scope='col'> LUGAR DE TRABAJO </th>  <th scope='col'> TELEFONO TRABAJO </th> <th scope='col'> TELEFONO CASA </th> <th scope='col'> RELIGION </th></thead>";
 	    	while($row = $result->fetch_assoc()) {
 			$res=$res."<tr>
 	    		<th scope=row><center> ". $row["MORRO_Boleta_C"]." </center></td>
 	    		<td><center> " . $row["NOM_C"]." ".$row["APE_PAT_C"]." ".$row["APE_MAT_C"]. " </center></td>
-	    		<td><center><img src='img/" . $row["FOTO_C"]. "' width=75%/></center></td>
+	    		<td><center><img src='img/customerImages/" . $row["FOTO_C"]. "' width=75%/></center></td>
 	    		<td><center>" .$row["CP_C"]." ".$row["ESTADO_C"]." ".$row["MUNICIPIO_C"]." ".$row["COLONIA_C"]." ".$row["NUM_EXT_C"]." ".$row["NUM_INT_C"]. "</center></td> 
 	    		<td><center>" .$row["LUGART_C"]. "</center></td> 
 	    		<td><center>" .$row["TELT_C"]." ".$row["EXT_C"]. "</center></td>
 	    		<td><center>" .$row["TELC_C"]. "</center></td>
-	    		<td><center>" .$row["RELIGION_C"]. "</center></td>
-	    		<td><center>" . $row["CURP_C"] . " </center></td>
-	    	  </tr>";
+	    		<td><center>" .$row["RELIGION_C"]. "</center></td>";
 	  		}    	/**/
 	    }
   		$res=$res."</table></center></div>";
 	}else{ /*error*/
-		$res="BD sin morros xd";
+		$res="BASE SIN DATOS";
 	}
 	echo $res;
 ?>
